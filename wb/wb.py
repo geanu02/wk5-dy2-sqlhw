0ff983a012696d46b797bb3f59928c18d8949876
Gian Carlo Torres <geanu@outlook.com>
2023-05-02 10:40:41 -0400

    Initial commit

---
+++ wb/wb.py
@@ -0,0 +1,8 @@
+#Write a function called getSumOfDigits that takes a positive integer as an argument and calculates the sum of its digits. You can assume that the argument is always a positive integer.
+
+Examples
+
+
+getSumOfDigits(123)  # Should return 6 (1 + 2 + 3 = 6)
+getSumOfDigits(223)  # Should return 7 (2 + 2 + 3 = 7)
+getSumOfDigits(1337) # Should return 14 (1 + 3 + 3 + 7 = 14)

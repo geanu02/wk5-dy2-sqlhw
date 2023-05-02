aee19a8953ac3be6bc918f94976a25307ac17202
Gian Carlo Torres <geanu@outlook.com>
2023-05-02 11:47:14 -0400

    Whiteboard complete.

---
+++ wb/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+    n_sum = 0
+    for i in range(len(n_str)):
+        n_sum += num[i]
+    return n_sum
--- wb/wb.py
+++ wb/wb.py
@@ -1,8 +1,17 @@
-#Write a function called getSumOfDigits that takes a positive integer as an argument and calculates the sum of its digits. You can assume that the argument is always a positive integer.
+# #Write a function called getSumOfDigits that takes a positive integer as an argument and calculates the sum of its digits. You can assume that the argument is always a positive integer.
 
-Examples
+# Examples
 
 
-getSumOfDigits(123)  # Should return 6 (1 + 2 + 3 = 6)
-getSumOfDigits(223)  # Should return 7 (2 + 2 + 3 = 7)
-getSumOfDigits(1337) # Should return 14 (1 + 3 + 3 + 7 = 14)
+# getSumOfDigits(123)  # Should return 6 (1 + 2 + 3 = 6)
+# getSumOfDigits(223)  # Should return 7 (2 + 2 + 3 = 7)
+# getSumOfDigits(1337) # Should return 14 (1 + 3 + 3 + 7 = 14)
+
+def getSumOfDigits(num):
+    n_str = str(num)
+    n_sum = 0
+    for i in range(len(n_str)):
+        n_sum += int(n_str[i])
+    return n_sum
+
+print(getSumOfDigits(1337))
